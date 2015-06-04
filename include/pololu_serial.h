c044010955fe2e597d442c1e31409563a3c7502c
Troy Dack <troy@dack.com.au>
2015-06-04 22:47:33 +1000

    Add license

--- include/pololu_serial.h
+++ include/pololu_serial.h
@@ -1,13 +1,25 @@
-/*
- * pololu_serial.h
+/**
+ *! @file pololu_serial.h
+ *! Pololu Simple Motor Controller (SMC)
+ *!
+ *! @author Troy Dack
+ *! @date Copyright (C) 2015
  *
- *  Created on: 21 May 2015
- *      Author: troy
- */
-/*! \file pololu_serial.h
- * \brief Pololu SMC header file.
- * Contains all of the method declarations for class SMC.
-**/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ **/
 
 #ifndef INCLUDE_POLOLU_SERIAL_H_
 #define INCLUDE_POLOLU_SERIAL_H_
