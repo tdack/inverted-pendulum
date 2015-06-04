84e13e7efe52d6258e8d0830179af53ceb980fbb
Troy Dack <troy@dack.com.au>
2015-06-04 23:23:14 +1000

    Fix merge conflicts

--- src/pololu_serial.cpp
+++ src/pololu_serial.cpp
@@ -5,7 +5,6 @@
  *! @author Troy Dack
  *! @date Copyright (C) 2015
  *
-<<<<<<< HEAD
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -21,11 +20,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  **/
-=======
- *  Created on: 21 May 2015
- *      Author: Troy Dack
-**/
->>>>>>> threads
 
 #include <fcntl.h>
 #include <stdio.h>
