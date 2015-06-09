2f63dfad678f86c7f7378bf0f83119b8e1412f04
Troy Dack <troy@dack.com.au>
2015-06-10 00:40:29 +1000

    Added loading of device tree overlay for eQEP1

--- load_overlays.sh
+++ load_overlays.sh
@@ -5,5 +5,7 @@ SLOTS=/sys/devices/bone_capemgr.9/slots
 echo "ADAFRUIT-UART1" > $SLOTS
 echo "PyBBIO-epwmss0" > $SLOTS
 echo "PyBBIO-eqep0" > $SLOTS
+echo "PyBBIO-epwmss1" > $SLOTS
+echo "PyBBIO-eqep1" > $SLOTS
 
 cat $SLOTS
