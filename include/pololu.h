fc2504741950162f0ab179ccad45d8cc805e6002
Troy Dack <troy@dack.com.au>
2015-07-07 12:02:58 +1000

    Pendulum encoder changed to 600ppr model giving 2400ppr in x4 mode

--- include/pololu.h
+++ include/pololu.h
@@ -42,8 +42,8 @@
 
 const double MOTOR_TEETH = 40.0;	// * Number of teeth on motor pulley
 const double ENCODER_TEETH = 12.0;	// * Number of teeth on encoder pulley
-const double ENCODER_PPR = 1600.0;	// * Encoder pulses per revolution (x4 mode)
-const double MOTOR_PPR = ENCODER_PPR * MOTOR_TEETH / ENCODER_TEETH;
+const double ENCODER_PPR = 2400.0;	// * Encoder pulses per revolution (x4 mode)
+const double MOTOR_PPR = 1600.0 * MOTOR_TEETH / ENCODER_TEETH;
 const char* const POLOLU_TTY = "/dev/ttyO2"; // * tty Pololu motor controller is on
 
 #endif /* INCLUDE_POLOLU_H_ */
