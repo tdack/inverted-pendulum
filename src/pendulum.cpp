0f2a6bf16c4635e1ebb00dbdba53db4740e24f4b
Troy Dack <troy@dack.com.au>
2015-07-09 07:41:13 +1000

    Basic websocket implementation, responds to unformatted messages.

--- src/pendulum.cpp
+++ src/pendulum.cpp
@@ -14,11 +14,204 @@
 
 using namespace std;
 
-void controller() {
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Net/WebSocket.h"
+#include "Poco/Net/NetException.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/HelpFormatter.h"
+#include "Poco/Format.h"
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+#include <iostream>
 
-	// Create new EQEPs object to monitor the pendulum & motor position
-	threadedEQEP *pendulumEQEP = new threadedEQEP(PENDULUM_EQEP, ENCODER_PPR);
-	threadedEQEP *motorEQEP = new threadedEQEP(MOTOR_EQEP, MOTOR_PPR);
+
+using Poco::Net::ServerSocket;
+using Poco::Net::WebSocket;
+using Poco::Net::WebSocketException;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::HTTPServerParams;
+using Poco::Timestamp;
+using Poco::ThreadPool;
+using Poco::Util::Option;
+using Poco::Util::OptionSet;
+using Poco::Util::HelpFormatter;
+using namespace Poco::JSON;
+
+//@TODO Get rid of nasty global variables. Use Poco::Application maybe?
+// Create new EQEPs object to monitor the pendulum & motor position
+threadedEQEP *pendulumEQEP = new threadedEQEP(PENDULUM_EQEP, ENCODER_PPR);
+threadedEQEP *motorEQEP = new threadedEQEP(MOTOR_EQEP, MOTOR_PPR);
+pid *Controller;
+
+string GetValue(Object::Ptr aoJsonObject, const char *aszKey) {
+    Poco::Dynamic::Var loVariable;
+    string lsReturn;
+    string lsKey(aszKey);
+
+    // Get the member Variable
+    //
+    loVariable = aoJsonObject->get(lsKey);
+
+    // Get the Value from the Variable
+    //
+    lsReturn = loVariable.convert<std::string>();
+
+    return lsReturn;
+}
+
+
+class PageRequestHandler: public HTTPRequestHandler
+	/// Return a HTML document with some JavaScript creating
+	/// a WebSocket connection.
+{
+public:
+	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
+	{
+		response.setChunkedTransferEncoding(true);
+		response.setContentType("text/html");
+		std::ostream& ostr = response.send();
+		ostr << "<html>";
+		ostr << "<head>";
+		ostr << "<title>WebSocketServer</title>";
+		ostr << "<script type=\"text/javascript\">";
+		ostr << "function WebSocketTest()";
+		ostr << "{";
+		ostr << "  if (\"WebSocket\" in window)";
+		ostr << "  {";
+		ostr << "    var ws = new WebSocket(\"ws://" << request.serverAddress().toString() << "/ws\");";
+		ostr << "    ws.onopen = function()";
+		ostr << "      {";
+		ostr << "        ws.send(\"Hello, world!\");";
+		ostr << "      };";
+		ostr << "    ws.onmessage = function(evt)";
+		ostr << "      { ";
+		ostr << "        var msg = evt.data;";
+		ostr << "        alert(\"Message received: \" + msg);";
+		ostr << "        ws.close();";
+		ostr << "      };";
+		ostr << "    ws.onclose = function()";
+		ostr << "      { ";
+		ostr << "        alert(\"WebSocket closed.\");";
+		ostr << "      };";
+		ostr << "  }";
+		ostr << "  else";
+		ostr << "  {";
+		ostr << "     alert(\"This browser does not support WebSockets.\");";
+		ostr << "  }";
+		ostr << "}";
+		ostr << "</script>";
+		ostr << "</head>";
+		ostr << "<body>";
+		ostr << "  <h1>WebSocket Server</h1>";
+		ostr << "  <p><a href=\"javascript:WebSocketTest()\">Run WebSocket Script</a></p>";
+		ostr << "</body>";
+		ostr << "</html>";
+	}
+};
+
+
+class WebSocketRequestHandler: public HTTPRequestHandler
+	/// Handle a WebSocket connection.
+{
+public:
+	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
+	{
+		try
+		{
+			WebSocket ws(request, response);
+			cout << "WebSocket connection established." << endl;
+			char buffer[1024];
+			int flags;
+			int n;
+			do
+			{
+				n = ws.receiveFrame(buffer, sizeof(buffer), flags);
+				string buf(buffer);
+				string out;
+				if (buf.find("pendulum") != string::npos) {
+					out = std::to_string(pendulumEQEP->getAngleDeg());
+					ws.sendFrame(out.data(), out.length()+1);
+				} else if (buf.find("kp") != string::npos) {
+					out = std::to_string(Controller->getKP());
+					cout << "kp: " << out << endl;
+					ws.sendFrame(out.data(), out.length()+1);
+				} else if (buf.find("ki") != string::npos) {
+					out = std::to_string(Controller->getKI());
+					cout << "ki: " << out << endl;
+					ws.sendFrame(out.data(), out.length()+1);
+				} else if (buf.find("kd") != string::npos) {
+					out = std::to_string(Controller->getKD());
+					cout << "kd: " << out << endl;
+					ws.sendFrame(out.data(), out.length()+1);
+				} else {
+					ws.sendFrame(buffer, n, flags);
+				}
+				cout << "Frame received (length=" << std::to_string(n) << ", flags=" << std::hex << unsigned(flags) << ")." << endl;
+			}
+			while (n > 0 || (flags & WebSocket::FRAME_OP_BITMASK) != WebSocket::FRAME_OP_CLOSE);
+			cout << "WebSocket connection closed." << endl;
+		}
+		catch (WebSocketException& exc)
+		{
+			switch (exc.code())
+			{
+			case WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
+				response.set("Sec-WebSocket-Version", WebSocket::WEBSOCKET_VERSION);
+				// fallthrough
+			case WebSocket::WS_ERR_NO_HANDSHAKE:
+			case WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
+			case WebSocket::WS_ERR_HANDSHAKE_NO_KEY:
+				response.setStatusAndReason(HTTPResponse::HTTP_BAD_REQUEST);
+				response.setContentLength(0);
+				response.send();
+				break;
+			}
+		}
+	}
+};
+
+
+class RequestHandlerFactory: public HTTPRequestHandlerFactory
+{
+public:
+	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request)
+	{
+		cout << "Request from "
+			+ request.clientAddress().toString()
+			+ ": "
+			+ request.getMethod()
+			+ " "
+			+ request.getURI()
+			+ " "
+			+ request.getVersion() << endl;
+
+		for (HTTPServerRequest::ConstIterator it = request.begin(); it != request.end(); ++it)
+		{
+			cout << it->first + ": " + it->second << endl;
+		}
+
+		if(request.find("Upgrade") != request.end() && Poco::icompare(request["Upgrade"], "websocket") == 0)
+			return new WebSocketRequestHandler;
+		else
+			return new PageRequestHandler;
+	}
+};
+
+void controller() {
 
 	// Initialise display
 	BlackLib::BlackI2C *I2C_1 = new BlackLib::BlackI2C(BlackLib::I2C_1, 0x3c);
@@ -45,7 +238,7 @@ void controller() {
 	}
 
 	// Create a new PID controller thread
-	pid *Controller = new pid(11.7, 10, 8, 40, pendulumEQEP, motorEQEP);
+	Controller = new pid(11.7, 10, 8, 40, pendulumEQEP, motorEQEP);
 
 	Controller->run();
 
@@ -53,7 +246,7 @@ void controller() {
 	fx.setCursor(6,8);
 	fx.write("PID Running ....  ");
 	int count = 0;
-	while (count < 50)  {
+	while (count < 500)  {
 		count++;
 		fx.setCursor(18,24);
 		fx.write(to_string(pendulumEQEP->getAngleDeg()).c_str());
@@ -73,9 +266,9 @@ void controller() {
 	WAIT_THREAD_FINISH(pendulumEQEP);
 	WAIT_THREAD_FINISH(motorEQEP);
 
-	cout << "Done!" << endl;
 	return;
 }
+
 
 bool checkOverlays(){
 	std::vector<std::string> files = {
@@ -86,7 +279,6 @@ bool checkOverlays(){
 	struct stat buffer;
 	bool overlays_loaded = true;
 
-	rlutil::setColor(rlutil::RED);
 	for (std::string& file : files) {
 		if (stat(file.c_str(), &buffer) != 0) {
 			rlutil::setColor(rlutil::YELLOW);
@@ -99,73 +291,6 @@ bool checkOverlays(){
 	return overlays_loaded;
 }
 
-void motorTest() {
-	BlackLib::BlackGPIO P8_7(BlackLib::GPIO_66, BlackLib::output);
-	BlackLib::BlackGPIO P8_8(BlackLib::GPIO_67, BlackLib::output);
-
-	P8_7 << BlackLib::high;
-	P8_8 << BlackLib::high;
-
-	// Create a Simple Motor Controller object
-	Pololu::SMC *SMC = new Pololu::SMC(POLOLU_TTY);
-
-	SMC->SetTargetSpeed(0);
-
-	cout.setf(std::ios::fixed);
-	cout << "Voltage: " << std::setprecision(2) << SMC->GetVariable(23)/1e3 << endl;
-
-	usleep(500);
-	SMC->SetTargetSpeed(256);
-	P8_7 << BlackLib::low;
-	P8_8 << BlackLib::high;
-
-	sleep(3);
-	SMC->SetTargetSpeed(-256);
-	P8_7 << BlackLib::high;
-	P8_8 << BlackLib::low;
-
-	sleep(3);
-	SMC->SetTargetSpeed(0);
-
-	P8_7 << BlackLib::low;
-	P8_8 << BlackLib::low;
-
-	delete SMC;
-	return;
-}
-
-void testOLED() {
-	BlackLib::BlackI2C *I2C_1 = new BlackLib::BlackI2C(BlackLib::I2C_1, 0x3c);
-	SSD1306::SSD1306 oled(I2C_1, NULL, 64);
-
-	int X = oled.get_width();
-	int Y = oled.get_height();
-
-	int max = (X > Y) ? X : Y;
-
-	oled.begin();
-	SSD1306::gfx fx(oled);
-
-	fx.clearScreen();
-	fx.setTextColor(SSD1306::RGB::black, SSD1306::RGB::white);
-	fx.setTextSize(2.5);
-
-	fx.setCursor(0,0);
-	fx.write("Hello\nWorld!");
-
-	sleep(2);
-	oled.clear();
-	fx.drawLine(0,0,X,Y, SSD1306::RGB::black);
-	fx.drawLine(0,Y,X,0, SSD1306::RGB::black);
-	for (int i = 0; i < max/5 -1; i += 10) {
-		fx.drawRoundRect(i, i,(X - 2 * i),(Y - 2 * i),8, SSD1306::RGB::black);
-	}
-	fx.drawCircle(X/2,Y/2,max/4 - 1, SSD1306::RGB::black);
-	oled.refresh();
-
-	sleep(2);
-}
-
 int main(int argc, char const *argv[]) {
 
 	if (!checkOverlays()) {
@@ -174,8 +299,16 @@ int main(int argc, char const *argv[]) {
 		return -1;
 	}
 
-//	testOLED();
-//	motorTest();
+	ServerSocket svs(9980);
+	// set-up a HTTPServer instance
+	HTTPServer srv(new RequestHandlerFactory, svs, new HTTPServerParams);
+	// start the HTTPServer
+	srv.start();
 	controller();
+	sleep(120);
+
+	srv.stop();
+
+	cout << "Done!" << endl;
 	return 0;
 }
