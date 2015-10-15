26c8d5f309e5228aed0f988981d336394033cc4e
Troy Dack <troy@dack.com.au>
2015-10-15 19:48:39 +1100

    Corrected weightings for various PID versions

--- scripts/PID_Tuning.m
+++ scripts/PID_Tuning.m
@@ -9,7 +9,7 @@ figure
     title('Pendulum Angle vs Time');
     xlabel('Time (s)');
     ylabel('Pendulum Angle (degrees)');
-  
+
 T = mean(data(:,1)); % sample delta T
 Fs = 1/T;            % frequency of samples
 L=length(data(:,1)); % number of samples
@@ -22,17 +22,17 @@ Tu = 1/meanfreq(data(:,2), Fs);
 fprintf('Tu=%.4f  Ku=%.4f\n\n', Tu, Ku)
 
 % Pessen Integral Rule
-Kp = 0.7*Ku; Ki=2.5*Kp/Tu; Kd=3*Kp*Tu/20;
+Kp = 0.7*Ku; Ki=Ku*Tu/2.5; Kd=Ku*3*Tu/20;
 fprintf('Pessen\nKp=%.4f Ki=%.4f Kd=%.4f\n\n', Kp, Ki, Kd)
 
 % Some overshoot
-Kp = 0.33*Ku; Ki=2*Kp/Tu; Kd=Kp*Tu/3;
+Kp = Ku/3; Ki=Ku*Tu/2; Kd=Kp*Tu/3;
 fprintf('Overshoot\nKp=%.4f Ki=%.4f Kd=%.4f\n\n', Kp, Ki, Kd)
 
 % Classic PID
-Kp = 0.6*Ku; Ki=2/Tu; Kd=Tu/8;
+Kp = 0.6*Ku; Ki=Ku*Tu/2; Kd=Ku*Tu/8;
 fprintf('Classic\nKp=%.4f Ki=%.4f Kd=%.4f\n\n', Kp, Ki, Kd)
 
 % Z-N PI
-Kp = 0.45*Ku; Ki=1.2/Tu; Kd=0;
+Kp = 0.45*Ku; Ki=Ku*Tu/1.2; Kd=0;
 fprintf('Z-N PI\nKp=%.4f Ki=%.4f Kd=%.4f\n\n', Kp, Ki, Kd)
